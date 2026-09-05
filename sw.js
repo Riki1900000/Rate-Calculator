67abd674bdff0ba57f444de8ae9fe0f360b3340e
Riki1900000 <manikkarmakar873@gmail.com>
2026-09-05 18:52:36 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,30 @@
+const CACHE_NAME = 'rate-calc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('rate-calc-v1').then((cache) => cache.addAll(['./index.html', './manifest.json']))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => 
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
